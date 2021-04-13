debe0f3646f30ea1dc7018757d293b3b9f4bef90
rladudgus04 <totoyou1234@naver.com>
2021-04-13 20:40:40 +0900

    이미지 추가 , 스크롤 완성

--- MyPortfolio/app.js
+++ MyPortfolio/app.js
@@ -26,29 +26,43 @@ window.onload = function(){
     let status = false;
     $(".bar-click").click(function(){
         if(status != true){
-            $(".left-menu").stop().animate({'left' : '0'},200);
+            $(".left-menu").stop().animate({'right' : '0'},200);
             status = true;
         }else {
-            $(".left-menu").stop().animate({'left' : '-40%'}, 200);
+            $(".left-menu").stop().animate({'right' : '-40%'}, 200);
             status = false;
         }
     });
 
     $(".menu-close").click(function(){
         status = false;
-        $(".left-menu").stop().animate({'left' : '-40%'}, 200);
+        $(".left-menu").stop().animate({'right' : '-40%'}, 200);
     });
     
-    // $('.slick-container').slick({
-    //     speed: 1000,
-    //     fade: true,
-    //     arrows: true
-    // });
+    $(document).ready(function(){
+        $('.slick-container').slick({
+            speed: 1000,
+            fade: true,
+            arrows: true
+        });
+        
+    });
+    
     
     $(".left-menu").click(function(e){
         console.log(e.target.dataset.menu);
         var offset = $(`.${e.target.dataset.menu}`).offset();
         $("html").animate({'scrollTop' : `${offset.top}`}, 400);
         console.log(offset.top);
+    });
+
+    var menuP =  $(".menu-project-list");
+
+    menuP.hide();
+
+    $(".pro").hover(function(){
+        menuP.fadeIn(100);
+    }, function(){
+        menuP.fadeOut(100);
     });
 };
